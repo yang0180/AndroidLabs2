93fba5db01976590dd105c9ccfd42b86a4e65cd8
yang <sagashgreas>
2021-01-26 22:22:21 -0500

    step2

--- app/src/main/java/com/cst2335/yang0180/MainActivity.java
+++ app/src/main/java/com/cst2335/yang0180/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
         Button btn =findViewById(R.id.btn);
         btn.setOnClickListener(e->{
             Toast.makeText(this,R.string.information,Toast.LENGTH_LONG);});
